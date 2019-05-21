9e8df3c900c26c8a963cd41cc7157c190c31751b
Josiah Turnquist <35823548+jturnquist276@users.noreply.github.com>
2019-05-20 23:23:04 -0700

    Update Pong.asm

--- Pong.asm
+++ Pong.asm
@@ -6,7 +6,7 @@ include Pong.inc
 ; game board and frame data
 BOARD_TOP_OFFSET equ 5d
 BOARD_LEFT_EDGE_OFFSET equ 15d
-BOARD_WIDTH equ 70d
+BOARD_WIDTH equ 72d
 BOARD_HEIGHT equ 20d
 BORDER_WIDTH equ 1d
 GUI_TEXT_COLOR equ (white)
@@ -14,7 +14,7 @@ GUI_COLOR equ (blue * 16)
 PADDLE_COLOR equ (white * 16)
 BALL_COLOR equ (white * 16)
 
-FRAME_RATE equ 50d
+FRAME_RATE equ 150d
 
 .data
 welcome byte "hey", 0
@@ -27,7 +27,7 @@ roomLowerBorder dword (BOARD_TOP_OFFSET + BOARD_HEIGHT)
 ; ball and paddle tracking
 xCoordBall dword BOARD_LEFT_EDGE_OFFSET
 yCoordBall dword BOARD_TOP_OFFSET + 2
-xRun dword 2
+xRun dword 1
 yRise dword 1
 
 ; character
@@ -40,15 +40,13 @@ player2Y dword (BOARD_TOP_OFFSET + (BOARD_HEIGHT / 2))
 
 paddleHeight dword 4h
 
-ballDirection dword 90				; in degrees? or slope? 
-
 playerColor dword (lightBlue * 16)
 ballColor dword (lightBlue * 16)
 guiColor dword (blue * 16)
 
 ; gui information
-p1scoreString byte "Player 1 score:", 0
-p2scoreString byte "Player 2 score:", 0
+p1scoreString byte "Player 1 score: ", 0
+p2scoreString byte "Player 2 score: ", 0
 p1score dword 0
 p2score dword 0
 
@@ -62,15 +60,21 @@ main proc
 
      mov ecx, 1
 MainLoop:
-	 invoke UpdateBall, addr xCoordBall, addr yCoordBall, BALL_COLOR, addr xRun, addr yRise, addr space, BOARD_TOP_OFFSET, 
-                        BOARD_HEIGHT, player1X, player1Y, player2X, player2Y, paddleHeight
-     inc ecx ; increment ecx to keep the loop going...when the ball goes out of bounds, set ecx to 0 so the inner loop can finish
+     invoke UpdateBall, addr xCoordBall, addr yCoordBall, BALL_COLOR, addr xRun, addr yRise, addr space, BOARD_TOP_OFFSET, BOARD_HEIGHT, player1X, player1Y, player2X, player2Y, paddleHeight     
+     
+     inc ecx                                           ; increment ecx to keep the loop going...when the ball goes out of bounds, set ecx to 0 so the inner loop can finish
      invoke Chill, FRAME_RATE
 
-     ; check for movement and redraw paddle accordingly
-     invoke CheckMovement, PADDLE_COLOR, addr player1x, addr player1Y, addr player2X, addr player2y, paddleHeight, 
-                            roomUpperBorder, roomLowerBorder
-	jmp MainLoop
+                                                       ; check for movement and redraw paddle accordingly
+     invoke CheckMovement, PADDLE_COLOR, addr player1x, addr player1Y, addr player2X, addr player2y, paddleHeight, roomUpperBorder, roomLowerBorder
+	jmp ML3
+ML2:
+    jmp MainLoop 
+ML3:
+
+     invoke DrawScore, GUI_TEXT_COLOR, GUI_COLOR, BOARD_TOP_OFFSET, BOARD_LEFT_EDGE_OFFSET, BOARD_WIDTH, BOARD_HEIGHT, BORDER_WIDTH, addr space, p1score, p2score, addr p1scoreString, addr p2scoreString 
+     
+     loop ML2
 	
 	
      exit
